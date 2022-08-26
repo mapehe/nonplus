dab3e6e54a65d556267e765ae8717f1b94f0b681
Matias Heikkilä <mapehe@iki.fi>
2022-08-26 20:07:32 +0300

    feat: basic queries

---
+++ src/select.ts
@@ -0,0 +1,51 @@
+export interface WhereQuery {
+  toString: () => string;
+  and: (whereStatement: string) => WhereQuery;
+}
+
+export interface Query {
+  toString: () => string;
+  where: (whereStatement: string) => WhereQuery;
+}
+
+export interface IncompleteQuery {
+  from: (table: string) => Query;
+}
+
+const toStringFactory =
+  (terms: string[], tableName: string, whereStatement?: string) => () =>
+    `SELECT ${terms.join(", ")} FROM ${tableName}${
+      whereStatement ? ` WHERE ${whereStatement}` : ""
+    }`;
+
+const whereQueryFacory = (
+  columns: string[],
+  table: string,
+  whereStatement: string
+): WhereQuery => ({
+  toString: toStringFactory(columns, table, whereStatement),
+  and: (andStatement: string) =>
+    whereQueryFacory(columns, table, `${whereStatement} AND ${andStatement}`),
+});
+
+const whereFactory =
+  (columns: string[], table: string) => (whereStatement: string) => ({
+    toString: toStringFactory(columns, table, whereStatement),
+    and: (andStatement: string) =>
+      whereQueryFacory(columns, table, `${whereStatement} AND ${andStatement}`),
+  });
+
+const fromFactory =
+  (columns: string[]) =>
+  (table: string): Query => ({
+    toString: toStringFactory(columns, table),
+    where: whereFactory(columns, table),
+  });
+
+export const all: IncompleteQuery = {
+  from: fromFactory(["*"]),
+};
+
+export const select = (...columns: string[]): IncompleteQuery => ({
+  from: fromFactory(columns),
+});
