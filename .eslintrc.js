218b88b865e8dcaa78241b2df3abb8eec0b8bc73
Matias Heikkilä <mapehe@iki.fi>
2022-08-26 19:21:08 +0300

    chore: set up the project

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "strict",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["bin"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    quotes: 0,
+    semi: 0,
+    indent: 0,
+    "template-curly-spacing": 0,
+    "array-bracket-spacing": 0,
+    "func-style": 0,
+    "array-element-newline": 0,
+    "no-confusing-arrow": 0,
+    "comma-dangle": 0,
+    "no-undefined": 0,
+    complexity: 0,
+    "filenames/match-regex": 0,
+    "id-blacklist": 0,
+    "operator-linebreak": 0,
+    "no-mixed-operators": 0,
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    "no-magic-numbers": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ test/test.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("sum module", () => {
+  test("adds 1 + 1 to equal 2", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
