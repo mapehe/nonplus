bad1348a4137d3ae54889c0f5ce0ea908159f4e7
Matias Heikkilä <mapehe@iki.fi>
2022-08-27 11:23:19 +0300

    test: validate test queries against sqlite (#10)

--- test/select.test.ts
+++ test/select.test.ts
@@ -1,56 +1,60 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test, beforeAll } from "@jest/globals";
 import { all, select } from "../src/select";
+import sqlite3 from "sqlite3";
 
 describe("Table", () => {
+  const db = new sqlite3.Database(":memory:");
+
+  const table = "people";
+  const first = "age";
+  const second = "height";
+  const third = "sex";
+  const lowerLimit = 10;
+  const upperLimit = 60;
+  const upperLimit2 = 180;
+  const like = "M";
+
+  beforeAll(() => {
+    db.exec(
+      `CREATE TABLE ${table} (${first} NUMBER, ${second} NUMBER, ${third} TEXT)`
+    );
+    db.exec(
+      `INSERT INTO ${table} (${first}, ${second}, ${third}) VALUES (25, 180, 'M')`
+    );
+  });
+
   describe("basic", () => {
     test("a simple query can be created", () => {
-      const table = "table";
-      const first = "age";
-      const second = "height";
-      const third = "sex";
-
       const expected = `SELECT ${first}, ${second}, ${third} FROM ${table}`;
       const query = select(first, second, third).from(table).render();
 
       expect(query).toBe(expected);
+      db.get(query);
     });
 
     test("a 'select *' can be created", () => {
-      const table = "table";
-
       const expected = `SELECT * FROM ${table}`;
       const query = all.from(table).render();
 
       expect(query).toBe(expected);
+      db.get(query);
     });
   });
 
   describe("where-clauses", () => {
     test("a where-clause can be created", () => {
-      const table = "table";
-      const first = "age";
-      const second = "height";
-      const third = "sex";
-      const limit = 10;
-
-      const expected = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${limit}`;
+      const expected = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit}`;
       const query = select(first, second, third)
         .from(table)
-        .where(`${first} > ${limit}`)
+        .where(`${first} > ${lowerLimit}`)
         .render();
 
       expect(query).toBe(expected);
+      db.get(query);
     });
 
     describe("and", () => {
       test("where-clauses can be combined with and", () => {
-        const table = "table";
-        const first = "age";
-        const second = "height";
-        const third = "sex";
-        const lowerLimit = 10;
-        const upperLimit = 60;
-
         // eslint-disable-next-line max-len
         const expected = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} AND ${first} < ${upperLimit}`;
         const query = select(first, second, third)
@@ -60,18 +64,10 @@ describe("Table", () => {
           .render();
 
         expect(query).toBe(expected);
+        db.get(query);
       });
 
       test("where-clauses can be combined with multiple ands", () => {
-        const table = "table";
-        const first = "age";
-        const second = "height";
-        const third = "sex";
-        const lowerLimit = 10;
-        const upperLimit = 60;
-        const upperLimit2 = 180;
-        const like = "M";
-
         // eslint-disable-next-line max-len
         const expected = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} AND ${first} < ${upperLimit} AND ${second} < ${upperLimit2}`;
         const query = select(first, second, third)
@@ -82,30 +78,25 @@ describe("Table", () => {
           .render();
 
         expect(query).toBe(expected);
+        db.get(query);
 
         // eslint-disable-next-line max-len
-        const expected2 = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} AND ${first} < ${upperLimit} AND ${second} < ${upperLimit2} AND ${third} LIKE ${like}`;
+        const expected2 = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} AND ${first} < ${upperLimit} AND ${second} < ${upperLimit2} AND ${third} LIKE '${like}'`;
         const query2 = select(first, second, third)
           .from(table)
           .where(`${first} > ${lowerLimit}`)
           .and(`${first} < ${upperLimit}`)
           .and(`${second} < ${upperLimit2}`)
-          .and(`${third} LIKE ${like}`)
+          .and(`${third} LIKE '${like}'`)
           .render();
 
         expect(query2).toBe(expected2);
+        db.get(query2);
       });
     });
 
     describe("or", () => {
       test("where-clauses can be combined with or", () => {
-        const table = "table";
-        const first = "age";
-        const second = "height";
-        const third = "sex";
-        const lowerLimit = 10;
-        const upperLimit = 60;
-
         // eslint-disable-next-line max-len
         const expected = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} OR ${first} < ${upperLimit}`;
         const query = select(first, second, third)
@@ -115,18 +106,10 @@ describe("Table", () => {
           .render();
 
         expect(query).toBe(expected);
+        db.get(query);
       });
 
       test("where-clauses can be combined with multiple ors", () => {
-        const table = "table";
-        const first = "age";
-        const second = "height";
-        const third = "sex";
-        const lowerLimit = 10;
-        const upperLimit = 60;
-        const upperLimit2 = 180;
-        const like = "M";
-
         // eslint-disable-next-line max-len
         const expected = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} OR ${first} < ${upperLimit} OR ${second} < ${upperLimit2}`;
         const query = select(first, second, third)
@@ -137,31 +120,24 @@ describe("Table", () => {
           .render();
 
         expect(query).toBe(expected);
+        db.get(query);
 
         // eslint-disable-next-line max-len
-        const expected2 = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} OR ${first} < ${upperLimit} OR ${second} < ${upperLimit2} OR ${third} LIKE ${like}`;
+        const expected2 = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} OR ${first} < ${upperLimit} OR ${second} < ${upperLimit2} OR ${third} LIKE '${like}'`;
         const query2 = select(first, second, third)
           .from(table)
           .where(`${first} > ${lowerLimit}`)
           .or(`${first} < ${upperLimit}`)
           .or(`${second} < ${upperLimit2}`)
-          .or(`${third} LIKE ${like}`)
+          .or(`${third} LIKE '${like}'`)
           .render();
 
         expect(query2).toBe(expected2);
+        db.get(query2);
       });
     });
     describe("and/or", () => {
       test("where-clauses can be combined with ors and ands", () => {
-        const table = "table";
-        const first = "age";
-        const second = "height";
-        const third = "sex";
-        const lowerLimit = 10;
-        const upperLimit = 60;
-        const upperLimit2 = 180;
-        const like = "M";
-
         // eslint-disable-next-line max-len
         const expected = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} OR ${first} < ${upperLimit} AND ${second} < ${upperLimit2}`;
         const query = select(first, second, third)
@@ -172,18 +148,20 @@ describe("Table", () => {
           .render();
 
         expect(query).toBe(expected);
+        db.get(query);
 
         // eslint-disable-next-line max-len
-        const expected2 = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} AND ${first} < ${upperLimit} OR ${second} < ${upperLimit2} OR ${third} LIKE ${like}`;
+        const expected2 = `SELECT ${first}, ${second}, ${third} FROM ${table} WHERE ${first} > ${lowerLimit} AND ${first} < ${upperLimit} OR ${second} < ${upperLimit2} OR ${third} LIKE '${like}'`;
         const query2 = select(first, second, third)
           .from(table)
           .where(`${first} > ${lowerLimit}`)
           .and(`${first} < ${upperLimit}`)
           .or(`${second} < ${upperLimit2}`)
-          .or(`${third} LIKE ${like}`)
+          .or(`${third} LIKE '${like}'`)
           .render();
 
         expect(query2).toBe(expected2);
+        db.get(query2);
       });
     });
   });
